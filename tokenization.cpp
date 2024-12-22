042ebe716627529163f38046471c840fc64926eb
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-12-22 23:22:23 +0600

    Updated Tokenization functionality

--- tokenization.cpp
+++ tokenization.cpp
@@ -16,7 +16,7 @@ void removeAllcomments(FILE *input_file,FILE *output_file)
             next_character=fgetc(input_file);
             if(next_character =='/')
             {
-                while((character== fgetc(input_file) != EOF && character != '\n')){}
+                while((character= fgetc(input_file) )!= EOF && character != '\n'){}
                 if(character == '\n')
                 {
                     fputc(character,output_file);
@@ -38,7 +38,58 @@ void removeAllcomments(FILE *input_file,FILE *output_file)
 
                     }
                 }
+            }else 
+            {
+                fputc(character,output_file);
+                fputc(next_character,output_file);
             }
+        }  else 
+        {
+            fputc(character,output_file);
         }
     }
 } 
+string keyword[10]={"for","while","if","else","switch","do","case","break","continue","return","struct"};
+
+bool isNotkey(string str)
+{
+    int count =0;
+    for(int i=0;i<10;i++)
+    {
+        if(str == keyword[i])
+        {
+            count=1;
+            break;
+        }
+    }
+    if(count == 0) return true;
+    else
+    return false;
+}
+bool is_valid_integer(string str)  //digit string
+{
+    bool flag=false;
+    for(int i=0;i<str.size();i++)
+    {
+        if(str[i]>='0' && str[i]<='9')
+        return true;
+        else
+        {
+            flag=false;
+            break;
+        }
+    }
+    if(flag== true) 
+    return true;
+    else
+    return false;
+}
+bool is_valid_float(string str)
+{
+    int flag1=0,flag2=0;
+    for(int i=0;i<str.size();i++)
+    {
+        
+    }
+
+}
